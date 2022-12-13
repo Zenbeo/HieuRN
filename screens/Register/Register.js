c60f4d36c340bef8a0a0a8f5353a8616a5d49ea9
Zenbeo <trinhhieu130520@gmail.com>
2022-12-13 22:56:35 +0700

    Add/lan6

--- screens/Register/Register.js
+++ screens/Register/Register.js
@@ -13,152 +13,156 @@ import {
   Dimensions,
   Alert,
 } from 'react-native';
-import { fontSize, images } from '../../constaints';
-import { colors } from '../../constaints/colors';
+import {fontSize, images} from '../../constaints';
+import {colors} from '../../constaints/colors';
 
 import {isValidEmail, isValidPassword} from '../../utilies/validations';
 
+export default function Register({navigation}) {
+  //state
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassWord] = useState('');
+  const [errorCheckPassword, setCheckErrorPassWord] = useState('');
+  //state email/pass
+  const [email, setEmail] = useState('');
+  const [password, setPassWord] = useState('');
+  const [checkPassword, setCheckPassWord] = useState('');
 
-export default function Register({navigation}) { //state
-const [errorEmail, setErrorEmail] = useState('');
-const [errorPassword, setErrorPassWord] = useState('');
-//state email/pass
-const [email, setEmail] = useState('');
-const [password, setPassWord] = useState('');
-
-const [keyboardIsShow, setKeyboardIsShow] = useState(false);
-useEffect(() => {
-  Keyboard.addListener('keyboardDidShow', () => {
-    setKeyboardIsShow(true);
+  const [keyboardIsShow, setKeyboardIsShow] = useState(false);
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardIsShow(true);
+    });
+    Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardIsShow(false);
+    });
   });
-  Keyboard.addListener('keyboardDidHide', () => {
-    setKeyboardIsShow(false);
-  });
-});
-const UserNameHandler=text => {
-setErrorEmail(
-  isValidEmail(text) == true ? '' : 'Tài khoản không đúng',
-);
-setEmail(text);
-}
-const PasWordHandler=text => {
-// cập nhật thay đổi thông tin
-setErrorPassWord(
-  isValidPassword(text) == true
-    ? ''
-    : 'Mật khẩu phải trên 3 kí tự',
-);
-setPassWord(text);
-}
-// const user = useContext(ContextProvider);
-const NotifineHandler=() => Alert.alert('Bạn đăng kí thành công ','Mời bạn đăng nhập',[ { text: "OK", onPress: () => navigation.navigate('Login')}])
-return (
-  <KeyboardAvoidingView
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    style={{
-      flex: 100,
-      // flexDirection:'row',
-      backgroundColor: 'white',
-    }}>
-    <View style={styles.ViewBackground}>
-      {/* <Text style={styles.TextBackground}>ĐẶT ĐỒ UỐNG </Text> */}
-      <Image
-        tintColor="blue" //màu icons
-        source={images.computers}
-        style={styles.Image}
-      />
-    </View>
-    <View style={{margin: 20}} />
-    <View
+  const UserNameHandler = text => {
+    setErrorEmail(isValidEmail(text) == true ? '' : 'Tài khoản không đúng');
+    setEmail(text);
+  };
+  const PassWordHandler = text => {
+    // cập nhật thay đổi thông tin
+    setErrorPassWord(
+      isValidPassword(text) == true ? '' : 'Mật khẩu phải trên 3 kí tự',
+    );
+    setPassWord(text);
+  };
+  const CheckPassWordHandler = text => {
+    // cập nhật thay đổi thông tin
+    setCheckErrorPassWord(
+      isValidPassword(text) == true ? '' : 'Mật khẩu phải trên 3 kí tự',
+    );
+    setCheckPassWord(text);
+  };
+  // const user = useContext(ContextProvider);
+  const NotifineHandler = () =>
+    Alert.alert('Bạn đăng kí thành công ', 'Mời bạn đăng nhập', [
+      {text: 'OK', onPress: () => navigation.navigate('Login')},
+    ]);
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{
-        flex: 30,
+        flex: 100,
+        // flexDirection:'row',
+        backgroundColor: 'white',
       }}>
+      <View style={styles.ViewBackground}>
+        {/* <Text style={styles.TextBackground}>ĐẶT ĐỒ UỐNG </Text> */}
+        <Image
+          tintColor="blue" //màu icons
+          source={images.computers}
+          style={styles.Image}
+        />
+      </View>
+      <View style={{margin: 20}} />
       <View
         style={{
-          marginHorizontal: 15,
+          flex: 30,
         }}>
-        <Text style={styles.titleName}>Tài khoản: </Text>
-        <TextInput
-          onChangeText={UserNameHandler}
-          style={styles.userText}
-          placeholder="Nhập tài khoản của bạn"
-          placeholderTextColor={'rgba(0,0,0,0.6)'}
-          // value={'hieu@gmail.com'}
-          value={email}
-          // ref='userText'
-          // autoFocus={true}
-          // onEndEditing={(text)=>{this.refs.passwordText.focus()}}//
-        />
-        <View style={styles.ViewEmail} />
-        {<Text style={styles.ErrorText}>{errorEmail}</Text>}
-      </View>
-      <View
-        style={{
-          marginHorizontal: 15,
-        }}>
-        <Text style={styles.titleName}>Mật khẩu: </Text>
-        <TextInput
-          onChangeText={PasWordHandler}
-          style={styles.passwordText}
-          placeholder="Nhập mật khẩu của bạn "
-          placeholderTextColor={'rgba(0,0,0,0.6)'}
-          secureTextEntry={true} //tính bảo mật ****
-          // value={'hhhh'}
-          value={password}
-          // ref='passwordText'
-        />
-        <View style={styles.ViewPassword} />
-        {<Text style={styles.ErrorText}>{errorPassword}</Text>}
-      </View>
-      <View
-        style={{
-          marginHorizontal: 15,
-        }}>
-        <Text style={styles.titleName}>Xác nhận  mật khẩu: </Text>
-        <TextInput
-          onChangeText={PasWordHandler}
-          style={styles.passwordText}
-          placeholder="Nhập mật khẩu của bạn "
-          placeholderTextColor={'rgba(0,0,0,0.6)'}
-          secureTextEntry={true} //tính bảo mật ****
-          // value={'hhhh'}
-          value={password}
-          // ref='passwordText'
-        />
-        <View style={styles.ViewPassword} />
-        {<Text style={styles.ErrorText}>{errorPassword}</Text>}
-      </View>
-    </View>
-
-    {keyboardIsShow == false ? (
-      <View //nếu bàn phím ko hiện thì nút Login hiện
-        style={{
-          flex: 10,
-          // marginTop:35
-        }}>
-        <TouchableOpacity
-          onPress={NotifineHandler}
-          style={styles.TouchLogin}>
-          <Text style={styles.TextLogin}>Đăng ký</Text>
-        </TouchableOpacity>
-        <View style={styles.ViewDki}>
-          <Text
-            style={styles.TextDki}
-            onPress={() => navigation.goBack('Login')}>
-            Trở lại
-          </Text>
-          <View style={{borderWidth: 0.2, width: 80}} />
+        <View
+          style={{
+            marginHorizontal: 15,
+          }}>
+          <Text style={styles.titleName}>Tài khoản: </Text>
+          <TextInput
+            onChangeText={UserNameHandler}
+            style={styles.userText}
+            placeholder="Nhập tài khoản của bạn"
+            placeholderTextColor={'rgba(0,0,0,0.6)'}
+            // value={'hieu@gmail.com'}
+            value={email}
+            // ref='userText'
+            // autoFocus={true}
+            // onEndEditing={(text)=>{this.refs.passwordText.focus()}}//
+          />
+          <View style={styles.ViewEmail} />
+          {<Text style={styles.ErrorText}>{errorEmail}</Text>}
+        </View>
+        <View
+          style={{
+            marginHorizontal: 15,
+          }}>
+          <Text style={styles.titleName}>Mật khẩu: </Text>
+          <TextInput
+            onChangeText={PassWordHandler}
+            style={styles.passwordText}
+            placeholder="Nhập mật khẩu của bạn "
+            placeholderTextColor={'rgba(0,0,0,0.6)'}
+            secureTextEntry={true} //tính bảo mật ****
+            // value={'hhhh'}
+            value={password}
+            // ref='passwordText'
+          />
+          <View style={styles.ViewPassword} />
+          {<Text style={styles.ErrorText}>{errorPassword}</Text>}
+        </View>
+        <View
+          style={{
+            marginHorizontal: 15,
+          }}>
+          <Text style={styles.titleName}>Xác nhận mật khẩu: </Text>
+          <TextInput
+            onChangeText={CheckPassWordHandler}
+            style={styles.passwordText}
+            placeholder="Nhập mật khẩu của bạn "
+            placeholderTextColor={'rgba(0,0,0,0.6)'}
+            secureTextEntry={true} //tính bảo mật ****
+            // value={'hhhh'}
+            value={checkPassword}
+            // ref='passwordText'
+          />
+          <View style={styles.ViewPassword} />
+          {<Text style={styles.ErrorText}>{errorCheckPassword}</Text>}
         </View>
       </View>
-    ) : (
-      <View></View>
-    )}
 
-   
-  </KeyboardAvoidingView>
-);
-    }
-    const {width, height} = Dimensions.get('window');
+      {keyboardIsShow == false ? (
+        <View //nếu bàn phím ko hiện thì nút Login hiện
+          style={{
+            flex: 15,
+            // marginTop:35
+          }}>
+          <TouchableOpacity onPress={NotifineHandler} style={styles.TouchLogin}>
+            <Text style={styles.TextLogin}>Đăng ký</Text>
+          </TouchableOpacity>
+          <View style={styles.ViewDki}>
+            <Text
+              style={styles.TextDki}
+              onPress={() => navigation.goBack('Login')}>
+              Trở lại
+            </Text>
+            <View style={{borderWidth: 0.2, width: 60}} />
+          </View>
+        </View>
+      ) : (
+        <View></View>
+      )}
+    </KeyboardAvoidingView>
+  );
+}
+const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
   ViewBackground: {
     flex: 20,
